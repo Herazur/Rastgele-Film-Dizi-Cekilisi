3da8a60b3070eeeccbd50e7efa39099ac7aadb0d
FurkanGokay <FurkanGokay@DESKTOP-1UJNJA0>
2021-09-21 11:36:56 +0300

    Add project files.

---
+++ RastgeleF/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace RastgeleF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] filmler = new string[6];
+            filmler[0] = "Upload";
+            filmler[1] = "The OA";
+            filmler[2] = "The Good Place";
+            filmler[3] = "Black Saıls";
+            filmler[4] = "Veronica";
+            filmler[5] = "Hayalet Şehir";
+
+            for (int i = 0; i < filmler.Length; i++)
+            {
+                Console.WriteLine(filmler[i]);
+            }
+
+            Console.WriteLine("*************************");
+
+            Random rnd = new Random();
+            int rastgele = rnd.Next(0, filmler.Length);
+            Console.WriteLine("Çıkan Film : " + filmler[rastgele]);
+
+            Console.ReadKey();
+
+        }
+    }
+}
